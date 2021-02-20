d89cc9f78d159fa56cccf1d3e3b80e24ad012583
JulenBordonaba <julenbordonaba@gmail.com>
2021-02-20 23:10:08 +0100

    quiero ir a main

--- Brackeys Game Jam 2021.1/Assets/_MisAssets/Scripts/CursorManager.cs
+++ Brackeys Game Jam 2021.1/Assets/_MisAssets/Scripts/CursorManager.cs
@@ -2,17 +2,21 @@
 using System.Collections.Generic;
 using UnityEngine;
 
-public class CursorManager : MonoBehaviour
+public class CursorManager : Singleton<CursorManager>
 {
-    // Start is called before the first frame update
-    void Start()
-    {
-        
-    }
+    public Texture2D defaultCursor;
+    public Texture2D onClickCursor;
 
     // Update is called once per frame
     void Update()
     {
-        
+        if(Input.GetMouseButton(0))
+        {
+            Cursor.SetCursor(onClickCursor, new Vector2(10, 10), CursorMode.ForceSoftware);
+        }
+        else
+        {
+            Cursor.SetCursor(defaultCursor, new Vector2(10, 10), CursorMode.ForceSoftware );
+        }
     }
 }
